ebe67e881eddb37491e032ae22c4f617a372d2c8
lkt1296745546 <1296745546@qq.com>
2019-07-31 16:51:33 +0800

    飞机大战游戏基本功能完成

--- src/com/neuedu/main/Gameframe.java
+++ src/com/neuedu/main/Gameframe.java
@@ -1,32 +1,79 @@
 package com.neuedu.main;
 
-import com.neuedu.constant.Frameconstant;
-import com.neuedu.runtime.Background;
+import com.neuedu.constant.FrameConstant;
+import com.neuedu.runtime.*;
+import com.neuedu.util.ImageMap;
+
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-public class Gameframe extends Frame {
+public class GameFrame extends Frame {
 
+    //创建背景对象
     private Background background = new Background();
+
+    //创建飞机对象
+    private Plane plane = new Plane();
+
+//    private EnemyBullet enemyBullet = new EnemyBullet();
+
+    //创建子弹集合
+    public final List<Bullet> bulletList = new CopyOnWriteArrayList<>();
+
+    //敌方子弹集合
+    public final List<EnemyBullet> enemyBulletList = new CopyOnWriteArrayList<>();
+
+    //创建敌方飞机集合
+    public final List<EnemyPlane> enemyPlaneList = new CopyOnWriteArrayList<>();
+
+    public boolean gameOver = false;
 
     @Override
     public void paint(Graphics g) {
+        if (!gameOver) {
+
+
         background.draw(g);
+        plane.draw(g);
+        for (Bullet bullet : bulletList) {
+            bullet.draw(g);
+        }
+        for (EnemyBullet enemyBullet : enemyBulletList) {
+            enemyBullet.draw(g);
+        }
+        for (EnemyPlane enemyPlane : enemyPlaneList) {
+            enemyPlane.draw(g);
+        }
+        for (Bullet bullet : bulletList) {
+            bullet.collisionTesting(enemyPlaneList);
+        }
+        for (EnemyBullet enemyBullet : enemyBulletList) {
+            enemyBullet.collisionTesting(plane);
+        }
     }
 
+//        g.setColor(Color.RED);
+//        g.drawString("" + enemyPlaneList.size(),100,100);
+    }
     /**
      * 使用这个方法初始化窗口
      */
     public void init(){
         //设置好窗口尺寸
-    setSize(Frameconstant.FRAME_WIDTH, Frameconstant.FRAME_HEIGHT);
+    setSize(FrameConstant.FRAME_WIDTH, FrameConstant.FRAME_HEIGHT);
 
     //设置窗口居中
     setLocationRelativeTo(null);
 
     enableInputMethods(false);
+
+    setResizable(false);
 
     addWindowListener(new WindowAdapter() {
         @Override
@@ -35,10 +82,18 @@ public class Gameframe extends Frame {
         }
     });
 
+    //添加键盘监听
+    addKeyListener(new KeyAdapter() {
+        @Override
+        public void keyPressed(KeyEvent e) {
+        plane.keyPressed(e);
+        }
 
-
-
-
+        @Override
+        public void keyReleased(KeyEvent e) {
+        plane.keyReleased(e);
+        }
+    });
     new Thread() {
 
         @Override
@@ -55,6 +110,12 @@ public class Gameframe extends Frame {
         }
     }.start();
 
+    //游戏初始时添加一些敌方飞机
+        enemyPlaneList.add(new EnemyPlane(300,30, ImageMap.get("ep01")));
+        enemyPlaneList.add(new EnemyPlane(400,40, ImageMap.get("ep01")));
+        enemyPlaneList.add(new EnemyPlane(500,50, ImageMap.get("ep01")));
+        enemyPlaneList.add(new EnemyPlane(600,60, ImageMap.get("ep01")));
+
     setVisible(true);
     }
   private Image offScreenImage = null;//创建缓冲区
@@ -62,7 +123,7 @@ public class Gameframe extends Frame {
     @Override
     public void update(Graphics g){
         if (offScreenImage == null) {
-            offScreenImage = this.createImage(Frameconstant.FRAME_WIDTH, Frameconstant.FRAME_HEIGHT);
+            offScreenImage = this.createImage(FrameConstant.FRAME_WIDTH, FrameConstant.FRAME_HEIGHT);
         }
             Graphics goff = offScreenImage.getGraphics();//创建离线图片的实例，在图片缓冲区绘图
             paint(goff);
